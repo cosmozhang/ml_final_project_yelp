076e92ae1ccf79d5e2190029b028be02b6b60b3c
Cosmo Zhang <cosmozhang1988@gmail.com>
2014-10-10 23:52:51 -0400

    social net work constructed

---
+++ friendsnetwork.py
@@ -0,0 +1,27 @@
+#2014 Yelp Data Challenge 
+#Cosmo Zhang & Praveen @Purdue
+#!/usr/bin/python3
+# Filename:snetwork.py
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+f = open('../data/yelp_academic_dataset_user.json', 'r')
+g = open('./yelp_user_network.csv', 'w')
+data = f.readlines()
+
+lnklsdic={}
+
+for line in data:
+    #print line
+    data = json.loads(line)#.replace("\\n", ""))
+    lnklsdic[data["user_id"]]=data["friends"]
+    
+for k in lnklsdic:
+    g.write(k+ ": " +', '.join(lnklsdic[k])+ '\n')
+
+g.close()
+f.close()
